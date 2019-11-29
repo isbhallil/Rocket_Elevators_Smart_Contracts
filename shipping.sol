929744dd07c4bc90fab338b1c2eb49c87fdc9073
mo bhallil <is.bhallil@gmail.com>
2019-11-28 22:22:23 -0500

    refactor and add functionality

--- shipping.sol
+++ shipping.sol
@@ -1,81 +1,141 @@
 pragma solidity >=0.4.22 <0.6.0;
 
 contract ShippingContract {
+ 
+    /////  :> STRUCTS <:  ////////////////////////////////////////////////////////////////////////////////////////////////////
+   
+    struct Order {
+        uint256 _id;
+        uint256 itemsCount;
+        mapping(uint => Item) items;
+    }
+    
     struct Item {
-        uint _id;
-        string _name;
+        uint256 _id;
+        bytes32 _name;
         bool _isLoaded;
         bool _isWrapped;
         bool _isDelivered;
         bool _isCleared;
-        bool _isCertified;
+        bool _isCertified; 
     }
-
-    mapping(uint => Item) public items;
+    
+    
+    
+    /////  :> STATE <:  ////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    mapping (uint256 => Order) internal ordersList;
     uint256 itemsCount = 0;
-
-    function addItem( string memory _name ) public returns (uint, string memory, bool, bool, bool, bool, bool) {
-        incrementItemsCount();
-        items[itemsCount] = Item(itemsCount, _name, false, false, false, false, false);
-
-        return getItem(items[itemsCount]._id);
+    uint256 ordersCount = 0;
+    
+    
+    
+    /////  :> FUNCTIONS <:  ////////////////////////////////////////////////////////////////////////////////////////////////////    
+    
+    function createOrder() public returns (uint256) {
+        incrementOrdersCount();
+        ordersList[ordersCount] = Order(ordersCount, 0);
+        
+        return ordersCount;
     }
-
-
-    function wrap(uint256 itemId) public returns (uint, string memory, bool, bool, bool, bool, bool) {
-        Item storage item = items[itemId];
-        if (item._isWrapped == false && item._isLoaded == false){
-            item._isWrapped = true;
+    
+    function addItem(uint256 orderId, bytes32 _name ) public returns (uint256, bytes32, bool, bool, bool, bool, bool) {
+        uint256 currentItemIndexinOrder = ordersList[orderId].itemsCount + 1;
+        ordersList[orderId].items[currentItemIndexinOrder] = Item(itemsCount,  _name, false, false, false, false, false);
+        
+        return getItem(orderId, itemsCount);
+    }
+      
+    function recordAction(uint256 orderId, uint256 itemId, bytes32 action) public returns (uint256, bytes32, bool, bool, bool, bool, bool) {
+        Item storage item = ordersList[orderId].items[itemId];
+        
+        if (action == 'wrap' && item._isWrapped == false && item._isLoaded == false){
+            item._isWrapped = true; 
         }
-
-        return getItem(item._id);
-    }
-
-    function unWrap(uint256 itemId) public returns (uint256, string memory, bool, bool, bool, bool, bool){
-        Item storage item = items[itemId];
-        if (item._isWrapped == true && item._isLoaded == false){
+        else if (action == 'unwrap' && item._isWrapped == true && item._isLoaded == false){
             item._isWrapped = false;
         }
-
-        return getItem(item._id);
-    }
-
-    function load(uint256 itemId) public returns (uint256, string memory, bool, bool, bool, bool, bool){
-        Item storage item = items[itemId];
-        if (item._isLoaded == false && item._isWrapped == true){
+        else if (action == 'load' && item._isLoaded == false && item._isWrapped == true){
             item._isLoaded = true;
         }
-
-        return getItem(item._id);
-    }
-
-    function unLoad(uint256 itemId) public returns (uint256, string memory, bool, bool, bool, bool, bool){
-        Item storage item = items[itemId];
-        if (item._isLoaded == true && item._isWrapped == true){
+        else if (action =='unload' && item._isLoaded == true && item._isWrapped == true){
             item._isLoaded = false;
         }
-
-        return getItem(item._id);
-    }
-
-    function cerifyItem(uint256 itemId) public returns (uint256, string memory, bool, bool, bool, bool, bool){
-        Item storage item = items[itemId];
-        if (item._isCertified == false){
+        else if (action == 'certify' && item._isCertified == false){
             item._isCertified = true;
         }
-
-        return getItem(item._id);
+        else if (action == 'clear' && item._isCleared == false){
+            item._isCleared = true;
+        }
+        else if (action == 'deliver' && item._isDelivered == false && isOrderDeliverable(orderId) ){
+            item._isDelivered = true;
+        }
+        
+        return getItem(orderId, item._id);
     }
-
-    // utils
-
-    function getItem(uint256 itemId) public view returns (uint256, string memory, bool, bool, bool, bool, bool) {
-        Item storage i = items[itemId];
+    
+    
+    
+    /////  :> VIEWS & UTILITIES <:  ////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+    function getItem(uint256 orderId, uint256 itemId) public view returns (uint256, bytes32, bool, bool, bool, bool, bool) {
+        Item storage i = ordersList[orderId].items[itemId];
         return (i._id, i._name, i._isLoaded, i._isWrapped, i._isDelivered, i._isCleared, i._isCertified);
     }
-
-    function incrementItemsCount() private {
-        itemsCount += 1;
+    
+    function isOrderDelivered(uint256 orderId) public view returns (bool){
+        bool isDelivered = true;
+        
+        for (uint index = 0; index < ordersList[orderId].itemsCount; index++) {
+          Item storage item = ordersList[orderId].items[index];
+          if (item._isDelivered == false) {
+              isDelivered = false;
+          }
+        }
+        
+        return isDelivered;
     }
-
+    
+    function isOrderDeliverable(uint256 orderId) private view returns (bool){
+        bool isDeliverbale = true;
+        
+        for (uint index = 0; index < ordersList[orderId].itemsCount; index++) {
+          Item storage item = ordersList[orderId].items[index];
+         
+          if (isDeliverbale == false){
+              break;
+          }
+          
+          if (item._isCertified == false && isDeliverbale == true ) {
+              isDeliverbale = false;
+          } 
+          
+          if (item._isWrapped == false && isDeliverbale == true ) {
+              isDeliverbale = false;
+          } 
+          
+          if (item._isLoaded == false && isDeliverbale == true ) {
+              isDeliverbale = false;
+          }
+          
+          if (item._isCleared == false && isDeliverbale== true ) {
+              isDeliverbale = false;
+          } 
+        }
+        
+        return isDeliverbale;
+    }
+    
+    function getItemsCountInOrder(uint256 orderId) private view returns (uint256){
+        return ordersList[orderId].itemsCount;
+    }
+    
+    function incrementOrdersCount() private returns (uint256){
+        return ++ordersCount;
+    }
+    
+    function incrementItemsCount(uint256 orderId) private returns (uint256, uint256){
+        return(++itemsCount, ++ordersList[orderId].itemsCount);
+    }
+    
 }
